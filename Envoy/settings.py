78185a0cd3ef68d2660eb4582616792ef46bdc2b
Akshay-cita <edakkatakshay@gmail.com>
2021-02-03 12:03:13 +0530

    Postgres connected

--- Envoy/settings.py
+++ Envoy/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Envoy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Envoy',
+        'USER':'postgres',
+        'PASSWORD':'Akshay@123',
+        'HOST':'127.0.0.1',
+        'PORT':'5432'
     }
 }
 
